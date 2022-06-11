c85aac1ac5561b2c1a7a6caf5713caebadd11f71
amit <amitkm492@gmail.com>
2022-06-11 18:54:12 +0530

    linked pages

--- client/src/components/LoginPage/LoginPage.jsx
+++ client/src/components/LoginPage/LoginPage.jsx
@@ -1,27 +1,81 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
+import React, {useState} from 'react';
+import {   useNavigate } from 'react-router-dom'
 
-
-
+const axios = require('axios');
 
 export default function LoginPage() {
+    const [user,setuser]=useState({
+        email:'',
+        password:''
+    })
+     
+    
+       const change=useNavigate()
+   
+   function signuphandel(){
+   
+       change.push('/')
+   }
+   
+   //const dispatch = useDispatch();
+   function handel(e){
+   
+       const {name,value}=e.target
+   
+       setuser({
+   
+           ...user,
+           [name]:value
+       })
+   
+   
+   }
+   
+   function submit(e){
+   
+       e.preventDefault()
+      
+       axios.get("http://localhost:3004/users", {
+      
+           
+           
+           
+   
+   }).then(resp => {
+   
+       const data = resp.data;
+   
+       data.map(function(el){
+           if(el.email==user.email&& el.password==user.password){
+              // change.push("/")
+   
+             
+              change("/users");
+           }
+          
+           
+       })
+   }).catch(error => {
+       
+   return "error"
+   });
+   }
     return(
     <div className="form">
     <form>
       <div className="input-container">
         <label>Username </label>
-        <input type="text" name="uname" required />
+        <input type="mail" name="email" onChange={handel} required />
        
       </div>
       <div className="input-container">
         <label>Password </label>
-        <input type="password" name="pass" required />
+        <input type="password" name="password" onChange={handel} required />
        
       </div>
       <p>Already have an account? <a href='/register'style={{textDecoration:"none"}}>Sign Up</a></p>
       <div className="button-container">
-        <input type="submit" />
+        <input type="submit" onClick={submit} />
       </div>
      
     </form>
--- client/src/components/RegisterPage/RegisterPage.jsx
+++ client/src/components/RegisterPage/RegisterPage.jsx
@@ -1,21 +1,108 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import {  useNavigate } from 'react-router-dom'
 
+
+const axios = require('axios');
 function RegisterPage() {
+    const [user,setuser]=useState({
+        email:'',
+        password:'',
+        firstName:"",
+        lastName:"",
+
+    })
+   
+       const change=useNavigate()
+   
+   
+   
+   
+   function handel(e){
+   
+       const {name,value}=e.target
+   
+       setuser({
+   
+           ...user,
+           [name]:value
+       })
+   
+   
+   }
+   
+   function submit(e){
+   
+       e.preventDefault()
+       const w={
+           name:user.name,
+           email:user.email
+       }
+   
+       axios.post("http://localhost:3004/users", {
+      
+            
+             firstName:user.firstName,
+             lastName: user.lastName,
+             role: user.role,
+             email: user.email,
+             password:user.password,
+
+         
+           
+           
+   
+   }).then(resp => {
+       console.log(resp);
+       alert("You have been registered");
+       change("/");
+   }).catch(error => {
+   
+   return "error"
+   
+   
+   });
+   
+   
+   }
+   
   return (
     <form>
     <div className="input-container">
-      <label>Username </label>
-      <input type="text" name="uname" required />
+      <label>First Name </label>
+      <input type="text" name="firstName"onChange={handel} placeholder='Enter your first name' required />
      
     </div>
+
+    <div className="input-container">
+      <label>Last Name </label>
+      <input type="text" name="lastName"onChange={handel} placeholder='Enter your last name' required />
+     
+    </div>
+ 
+    <div className="input-container">
+      <label>Email </label>
+      <input type="mail" name="email"onChange={handel} placeholder='Enter your email' required />
+     
+    </div>
+
     <div className="input-container">
       <label>Password </label>
-      <input type="password" name="pass" required />
-     
+      <input type="password" name="password" onChange={handel} required />
     </div>
+
+    <div className="input-container">
+    <label >Role</label>
+    <input type="text" name="role" onChange={handel}  checked/>
+    
+
+    </div>
+
+
+
+
     <p>Already have an account? <a href='/' style={{textDecoration:"none"}}>Sign In</a></p>
     <div className="button-container">
-      <input type="submit" />
+      <input type="submit" onClick={submit} />
     </div>
    
   </form>
